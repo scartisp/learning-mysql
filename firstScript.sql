1279990fbc1b1360873d1fa9a3824a4f88cf1158
scartisp <simion.cartis1@gmail.com>
2026-06-06 16:49:20 -0400

    initial commit

---
+++ firstScript.sql
@@ -0,0 +1,10 @@
+# Simion Cartis
+SELECT * FROM parks_and_recreation.employee_demographics; # select everything from databse.table
+# keywords do not need to be capitalized, but it usually is convention 
+
+SELECT first_name,
+last_name,
+birth_date,
+age,
+age+10
+FROM parks_and_recreation.employee_demographics;
